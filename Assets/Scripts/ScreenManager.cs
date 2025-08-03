0fe15a1f4d06ff451b1be5841f790a3e18f923b4
Henrique Baldo Carlos <80064286+boldoboldin@users.noreply.github.com>
2025-08-03 10:37:40 -0300

    Scene "Praia" Recovery

--- Assets/Scripts/ScreenManager.cs
+++ Assets/Scripts/ScreenManager.cs
@@ -5,43 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class ScreenManager : MonoBehaviour
 {
-    [SerializeField] Animator uiAnim;
-    [SerializeField] GameObject credits;
-    [SerializeField] bool creditsShown = false;
-
     [SerializeField] private float actionDelay;
-    [SerializeField] private int scene;
-
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            ExitGame();
-            uiAnim.Play("WaterBillboardTransition");
-        }
-
-        if (Input.GetKeyDown(KeyCode.Return))
-        {
-            LoadScene(scene);
-            uiAnim.Play("WaterBillboardTransition");
-        }
-
-        if (Input.GetKeyDown(KeyCode.LeftControl))
-        {
-            if (creditsShown == false)
-            {
-                credits.SetActive(true);
-                creditsShown = true;
-                uiAnim.Play("WaterBillboardTransition");
-            }
-            else
-            {
-                credits.SetActive(false);
-                creditsShown = false;
-                uiAnim.Play("WaterBillboardTransitionHalfReverse");
-            }
-        }
-    }
 
     public void LoadScene(int scene)
     {
